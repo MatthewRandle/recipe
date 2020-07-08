22813340f4395c552c44a01ab23c6eac55dfba35
Matthew Randle <exportknowledge.matthew@gmail.com>
2020-07-08 10:35:10 +0100

    Made footer responsive

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ export default function MyApp({ Component, pageProps }) {
         <div>
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@800&display=swap" rel="stylesheet"></link>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1" />
             </Head>
 
             <Component {...pageProps} />
